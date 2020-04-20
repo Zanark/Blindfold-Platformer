013a25cba7b6ded0a0235d8382a5802dbee9f343
Zanark <kiodeba11@gmail.com>
2020-04-20 11:28:33 +0530

    Stuff aint working in C#

---
+++ GodotSrc/GrabTest.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public class GrabTest : Area2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    public void Area2DInputEvent(Viewport node, InputEvent inputEvent, int shapeLdx)
+    {
+        // if ()
+        // {
+            
+        // }
+    }
+
+    public override void _Process(float delta)
+    {
+        
+    }
+}
--- GodotSrc/Player.cs
+++ GodotSrc/Player.cs
@@ -11,18 +11,28 @@ public class Player : KinematicBody2D
 
     private Vector2 motion = Vector2.Zero;
 
+    public AnimationPlayer animationPlayer;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
     }
 
     public override void _PhysicsProcess(float delta)
     {
         motion.y += GRAVITY * delta;
+
+        if(motion.x <= 0)
+            animationPlayer.Play("Idle");
+        else if(motion.x > 0)
+            animationPlayer.Play("Walk");
+        else if(motion.y > 0)
+            animationPlayer.Play("Fall");
+
         motion.x = MAX_SPEED;
 
-        motion = MoveAndSlide(motion);
+        motion = MoveAndSlide(motion, Vector2.Up);
         
     }
 }
