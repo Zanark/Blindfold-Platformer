05171314cf487d992058e236167c80dace68afa4
Zanark <zanarkdev@gmail.com>
2020-04-27 18:00:46 +0530

    Added background

--- GodotSrc/Dragable.gd
+++ GodotSrc/Dragable.gd
@@ -1,11 +1,10 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 onready var dragMouse = false
+onready var GRAVITY = Vector2(0, 100)  # gravity force
+onready var VELOCITY = Vector2()  # the area's velocity
+var on_floor = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -18,14 +17,24 @@ func _on_Dragable_input_event(_viewport, event, _shape_idx):
 			dragMouse = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
 	if(dragMouse):
 		set_position(get_viewport().get_mouse_position())
+		on_floor = false
+		VELOCITY = Vector2.ZERO
+
+	if !on_floor:
+		VELOCITY += GRAVITY * delta
+		set_position(get_position() + VELOCITY * delta)
+	
 	
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player":
 			free()
+		else :
+			VELOCITY = Vector2.ZERO
+			on_floor = true
 
 
 func _on_Dragable_area_exited():
