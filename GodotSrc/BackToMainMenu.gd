784815d7abdbbb7a7a275f254b7ae64df9fc2e0e
Zanark <zanarkdev@gmail.com>
2021-04-12 21:19:41 +0530

    created a new branch for HTML export

--- GodotSrc/BackToMainMenu.gd
+++
@@ -1,4 +0,0 @@
-extends Button
-
-func _on_MainMenu_pressed():
-	get_tree().change_scene('res://TitleScreen.tscn')
--- GodotSrc/StartGame.gd
+++
@@ -1,4 +0,0 @@
-extends Button
-
-func _on_StartGame_pressed():
-	get_tree().change_scene('res://Level1.tscn')
